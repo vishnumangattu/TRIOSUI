c17d9f75f47798f60b37fc426b0fd9d082bf89d4
vishnumangattu <vishnumangattu03@gmail.com>
2025-03-27 10:39:24 +0530

    vishnu

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+function displayNav() {
+    document.getElementById('threedots').style.display = 'none';
+    document.getElementById('twolines').style.display = "flex";
+    // document.getElementById('').style.display = "flex";
+}
+function removeNavs() {
+    document.getElementById('threedots').style.display = 'block';
+    document.getElementById('twolines').style.display = "none";
+}
+
+function removeNav() {
+    document.getElementById('threedots').style.display = 'flex';
+    document.getElementById('twolines').style.display = "none";
+}
